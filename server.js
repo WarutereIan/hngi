88ae9096f061dc62c7fb60b35cff43941eb8bf93
WarutereIan <nmwanik111@gmail.com>
2022-11-07 13:31:47 +0300

    Update server.js
    update to handle post

--- server.js
+++ server.js
@@ -10,6 +10,54 @@ app.get('/',(req,res)=>{
 "bio":"I am an aspiring backend and blockchain developer with expertise in building MVC based servers"})
 })
 
+app.post('/', (req,res)=>{
+  const payload = req.body
+  console.log(payload)
+  const operation= payload.operation_type
+  const x = payload.x
+  const y = payload.y
+  
+  
+  if(operation == 'addition'){
+    const result = x + y
+    console.log(result)
+    res.send(
+    {
+      "slackUsername": "Warutere",
+      "result": result,
+      "operation_type": operation
+    })
+  }
+  else if(operation == 'subtraction' ){
+    const result = x - y
+    console.log(result)
+     res.send(
+    {
+      "slackUsername": "Warutere",
+      "result": result,
+      "operation_type": operation
+    })
+  }
+  else if(operation == 'multiplication'){
+    const result = x * y
+    console.log(result)
+     res.json(
+    {
+      "slackUsername": "Warutere",
+      "result": result,
+      "operation_type": operation
+    })
+  }
+  else{
+    return res.json(
+    {
+      "slackUsername": "Warutere",
+      "result": "result",
+      "operation_type": "operation"
+    })
+  }
+})
+
 app.listen(5000, (err)=>{
     if(err){
         return console.log(err)
